311763f7994bd57070d95889de5919120a639ee0
Diego Basch <dbasch@gmail.com>
2013-08-31 15:35:12 -0700

    made more concise and readable

--- src/closearch/core.clj
+++ src/closearch/core.clj
@@ -1,36 +1,36 @@
+;; CloSearch - a very basic text search engine
+
 (ns closearch.core
-  (:gen-class :main true)
-  (:use ring.adapter.jetty [clojure.set :only (union intersection)] [clojure.string :only (split lower-case)] [clj-time.core :only (now)]))
+  (:use ring.adapter.jetty [clojure.set :only (union intersection)] [clojure.string :only (split lower-case)]))
 
-(def p (re-pattern #"[^\p{L}&&[^\p{M}]]"))
-(def index)
+(defn tokenize [text]
+  (split (lower-case text) #"[\s#&!:,;\.\\\+-]+"))
 
-;; Read a file, invert it, and add it to the index.
-(defn invert[file]
-  (let [f (.getName file)
-        tokens (.split p (lower-case (slurp file)))]
-        (into {} (mapcat #(hash-map % #{f}) tokens))))
+;; Create token -> file mappings for all tokens in a text file
+(defn mappings [file]
+  (map #(hash-map % #{file}) (-> file slurp tokenize distinct)))
 
-;; Index all files.
-(defn build-index[dirname]
-  (reduce #(merge-with union %1 %2) (map invert (.listFiles (java.io.File. dirname)))))
+;; Stick all the mappings for all the tokens in all the files into a single in-memory map
+(def idx
+  (let [files (.listFiles (java.io.File. "text/"))]
+    (apply merge-with union (mapcat (comp mappings #(.getPath %)) files))))
 
 ;; An "AND" query. The only type we support.
-(defn search[q]
-  (reduce intersection (map (comp index lower-case) (split q #"\+"))))
+(defn search [q]
+  (apply intersection (map (comp idx lower-case) (split q #"\+"))))
 
-;; A quick and dirty way to render an html response for a query. Don't judge me :)
-(defn response[q]
+;; A quick and dirty way to render an html response for a query.
+(defn response [q]
   (str "<html>" (apply str (interpose "<br/>" (search q))) "</html>"))
 
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body (if (req :query-string) (response (req :query-string)))})
+   :body
+   (if (req :query-string) (response (req :query-string)))})
 
 ;; Run the search server.
-(defn -main[& args]
-  (println (str "Building index: " (now)))
-  (def index (build-index (first args)))
-  (println (str "Starting search server: " (now)))
+(defn boot []
   (run-jetty #'app {:port 8080 :join? false}))
+
+(defn -main[& args] (boot))
--- src/closearch/testfiles.clj
+++ src/closearch/testfiles.clj
@@ -1,15 +1,15 @@
 (ns closearch.testfiles
- (:gen-class :main true)
- (:import java.io.File)
- (:use [clojure.string :only (split)]))
+  (:gen-class :main true)
+  (:import java.io.File)
+  (:use [clojure.string :only (split)]))
 
 (def dict (split (slurp "/usr/share/dict/words") #"\n"))
 
 ;; Use this to generate a relatively large number of files for performance tests.
 ;; e.g. 10000
-(defn -main[& args]
- (.mkdir (new File (second args)))
- (let [nwords (count dict)]
- (dotimes [n (#(Integer/parseInt %) (first args))]
-   (let [positions (take 1000 (repeatedly #(rand-int nwords)))]
-  (spit (str (second args) "/file." n) (apply str (interpose " " (map (partial nth dict) positions))))))))
+(defn -main [& args]
+  (.mkdir (new File (second args)))
+  (let [nwords (count dict)]
+    (dotimes [n (#(Integer/parseInt %) (first args))]
+      (let [positions (take 1000 (repeatedly #(rand-int nwords)))]
+        (spit (str (second args) "/file." n) (apply str (interpose " " (map (partial nth dict) positions))))))))
